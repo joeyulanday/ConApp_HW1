3beadc6ab0a9f8e32b84987aa40234ec6055546d
Joey Anne Ulanday <joul@simcorp.com>
2024-10-09 15:34:00 +0800

    Added new line for feature branch 1

--- ConsoleApp_HW1/ConsoleApp_HW1/Program.cs
+++ ConsoleApp_HW1/ConsoleApp_HW1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp_HW1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Feature branch 1 - development 1");
         }
     }
 }
